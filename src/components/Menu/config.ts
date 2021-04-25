9212dcd9041923e68969c64eb16ed91e1bd6e313
Jaenudin Maulana Marpaung <resultlegal69@gmail.com>
2021-04-26 04:35:53 +0700

    Initial commit

---
+++ src/components/Menu/config.ts
@@ -0,0 +1,16 @@
+import { MenuEntry } from '@pancakeswap-libs/uikit'
+
+const config: MenuEntry[] = [
+  {
+    label: 'Home',
+    icon: 'HomeIcon',
+    href: 'https://swap.udin.dev/',
+  },
+  {
+    label: 'Trade',
+    icon: 'TradeIcon',
+    href: '/swap',
+  },
+]
+
+export default config
